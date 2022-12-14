16cc43c5e3cabe2cb6be1f9731a76b729188fb97
Xing <820239854@qq.com>
2022-12-14 11:06:18 +0800

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+```bash
+mkdir build
+cmake -S . -B build
+```
+
+项目属性-配置属性-链接器-系统：子系统（选控制台 (/SUBSYSTEM:CONSOLE)或 窗口 (/SUBSYSTEM:WINDOWS)）
+
+>  <SubSystem>Console</SubSystem> 改成  <SubSystem>Windows</SubSystem>
---
+++ src/Main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#pragma warning(push, 3)
+#include <windows.h>
+#pragma warning(pop)
+
+int __stdcall WinMain(HINSTANCE Instance, HINSTANCE PreviousInstance, PSTR CommandLine, INT CmdShow)
+{
+	system("pause");
+	return(0);
+}
